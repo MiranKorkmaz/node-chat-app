09550a7d59fc72360e82f47b899dbe0558bb5a1a
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-03-07 09:22:06 +0100

    pushing everything up to date

--- middlewear/middlewear.js
+++ middlewear/middlewear.js
@@ -1,9 +1,9 @@
 exports.reqLogin = (req, res, next) => {
-    //If user is logged in, continue to next middlewear
-    if (req.session && req.session.user) {
-      return next();
-    } else {
-      //If user is not logged in, redirect to login page
-      return res.redirect("/login");
-    }
-  };
+  //If user is logged in, continue to next middlewear
+  if (req.session && req.session.user) {
+    return next();
+  } else {
+    //If user is not logged in, redirect to login page
+    return res.redirect("/login");
+  }
+};
